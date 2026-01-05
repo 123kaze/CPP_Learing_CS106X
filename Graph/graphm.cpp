cac9d8aee1e3d19dc35e8622fa7d3a09978f95c4
123kaze <shengxuh5@gmail.com>
2026-01-05 16:20:06 +0800

    123

--- Graph/graphm.cpp
+++ Graph/graphm.cpp
@@ -1,19 +1,21 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
 #include <algorithm>
+#include <cmath>
+#include <iostream>
 #include <queue>
+#include <vector>
+
 #include "grapgh1.h"
 using namespace std;
 
-class Graphm:public Graph
+class Graphm : public Graph
 {
-private:
-    int numVertex,numEdge; //储存边和节点
-    int **matrix; //printer to adj martrix
-    int *mark;  //printer to mark array
-public:
-    Graphm(int numVert)  //构造函数
-    { Init(numVert);}
-    
+   private:
+    int numVertex, numEdge;  // 储存边和节点
+    int** matrix;            // printer to adj martrix
+    int* mark;               // printer to mark array
+   public:
+    Graphm(int numVert)  // 构造函数
+    {
+        Init(numVert);
+    }
 };
