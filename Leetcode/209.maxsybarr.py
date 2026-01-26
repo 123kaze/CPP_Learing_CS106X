2876b649b2e5e2536929dc4a1e0dc7d0a085faeb
123kaze <shengxuh5@gmail.com>
2026-01-26 17:34:03 +0800

    2026/01/26/ leetcode 209

---
+++ Leetcode/209.maxsybarr.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        sum1 = sum(nums)
+        if(target > sum1):
+            return 0
+        
+        cur = 0
+        j = 0
+        leng = 9999999999
+        res = 999999
+        for i in range(len(nums)):
+            cur = cur+nums[i]
+            while(cur >= target):
+                leng = (i-j+1) # sublength  外层循环是结束，内层是开始，通过不断加入，然后
+                #像一个窗口一样，划出，得到结果
+                res = leng if leng < res else res
+                cur -= nums[j]
+                j+=1
+
+        return res if res != 999999  else 0
+
