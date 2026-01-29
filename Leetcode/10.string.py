c5f4195442bbc7c6c20d7fe2970989c741dd8078
123kaza <shengxuh5@gmail.com>
2026-01-30 00:02:38 +0800

    tijiao

--- Leetcode/10.string.py
+++ Leetcode/10.string.py
@@ -31,3 +31,34 @@ class Solution:
                         dp[i][j] = dp[i-1][j-1]
 
         return dp[n][m]
+
+
+
+
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m = len(s)
+        n = len(p)
+
+        dp =[[False]*(n+1) for _ in range(m+1)]
+        dp[0][0] = True
+
+        for j in range(2,n+1):
+            if p[j-1] =='*':
+                dp[0][j] = dp[0][j-2]
+        
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if p[j-1] =='*':
+                    match1 = dp[i][j-2]
+                    match_not_zero = (s[i-1] == p[j-2] or p[j-2] == '.') and dp[i-1][j]
+                    mat2 = match1 or match_not_zero
+                    dp [i][j] = mat2
+                
+                else:
+                    if p[j-1] =='.' or p[j-1] == s[i-1]:
+                        dp[i][j] = dp[i-1][j-1]
+            
+        return dp[m][n]
+                    
