ec17950464e93d96c7f5018cfa227d5764bca81e
123kaze <shengxuh5@gmail.com>
2026-03-08 11:43:17 +0800

    Add Tree and TwoPointer problems - 二叉树与双指针
    新增四个算法题目：
    
    1. Leetcode/Tree/105.buildTree.py - 从前序与中序遍历序列构造二叉树
       * 题目：105. Construct Binary Tree from Preorder and Inorder Traversal
       * 算法：递归分治
       * 关键：前序第一个元素为根，在中序中找到根的位置划分左右子树
       * 时间复杂度：O(n^2) 最坏情况，可优化为O(n)使用哈希表
    
    2. Leetcode/Tree/114.flatten.py - 二叉树展开为链表
       * 题目：114. Flatten Binary Tree to Linked List
       * 算法：前序遍历 + 链表重建
       * 先序遍历存储节点，然后重新连接为右子树链表
       * 空间复杂度：O(n)
    
    3. Leetcode/Tree/236.lowestCommonAncestor_phrasie.py - 二叉树的最近公共祖先
       * 题目：236. Lowest Common Ancestor of a Binary Tree
       * 算法：递归后序遍历
       * 关键逻辑：
         - 找到p或q则返回当前节点
         - 左右子树都有结果则当前节点为LCA
         - 否则返回非空的那个结果
       * 时间复杂度：O(n)
    
    4. Leetcode/TwoPointer/threesum - 三数之和
       * 题目：15. 3Sum
       * 算法：排序 + 双指针
       * 关键优化：
         - 排序后固定第一个数
         - 使用双指针寻找另外两个数
         - 跳过重复元素避免重复解
       * 时间复杂度：O(n^2)

---
+++ Leetcode/Tree/105.buildTree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+from typing import Optional, List
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+        root = TreeNode(preorder[0])
+        i = inorder.index(root.val)
+        root.left = self.buildTree(preorder[1 : i + 1], inorder[:i])
+        root.right = self.buildTree(preorder[i + 1 :], inorder[i + 1 :])
+        return root
