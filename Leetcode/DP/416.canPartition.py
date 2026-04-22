38668b81d2b6ff3335fcf42e897e59119ebb0c53
123kaza <shengxuh5@gmail.com>
2026-04-22 22:19:03 +0800

    13

--- Leetcode/DP/416.canPartition.py
+++ Leetcode/DP/416.canPartition.py
@@ -3,12 +3,23 @@ from typing import List
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         n = len(nums)
-
+        s = sum(nums)
+        if s % 2 != 0:
+            return False
+        s = s // 2
         @cache
-        def dfs(i):
+        def dfs(i,j)->bool:
             '''
-            :param i:
+            边界：i==0,j==0
+            :param i: 当前第i个
+            :param j: target值
             :return:
+            dfs(i,j) = dfs(i-1,j) or dfs(i-1,j-nums[i])
             '''
-            if i == 0:
-                return False
+            if i <0 :
+                return True if j == 0 else False
+            if j < nums[i] :
+                return dfs(i-1,j)
+            return dfs(i-1,j) or dfs(i-1,j-nums[i])
+
+        return dfs(n-1,s)
